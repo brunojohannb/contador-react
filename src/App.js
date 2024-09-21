5332732389b1da6037c63f39daeda0299b035c73
Bruno Johann B. <brunojohannber@gmail.com>
2024-09-20 21:36:03 -0300

    Basic Construction

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount (count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Contador: {count}</h1>
+      <button onClick={increment}>Incrementar</button>
+      <button onClick={decrement}>Decrementar</button>
+      <button onClick={reset}>Resetar</button>
+    </div>
+  );
+
+};
+
+export default App;
+
+
+  
